53978e17d7418dc75418d987bf06dce07064fb71
tagdus <tagdus@yahoo.com>
2025-09-10 11:38:29 +0000

    Main commit.

---
+++ cmd/erigon/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  erigonapp "github.com/tagdus/erigon/turbo/app"
+)
+
+func main() {
+  fmt.Println("Hello, World from main()!")
+  erigonapp.MakeApp()
+}
---
+++ turbo/app/foobar.go
@@ -0,0 +1,9 @@
+package app
+
+import (
+  "fmt"
+)
+
+func MakeApp() {
+  fmt.Println("Hello, World from MakeApp()!")
+}
